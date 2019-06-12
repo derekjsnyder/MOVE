3ac513bc72b7a730b4326d384f159814cc088ec9
Derek Snyder <dsnyder@healthwise.org>
2019-06-12 16:09:11 -0600

    Broken Styled Components

--- Movement/ClientApp/src/components/exercise.js
+++ Movement/ClientApp/src/components/exercise.js
@@ -1,9 +1,20 @@
 import * as React from "react";
+import styled from "styled-components";
 
 class exercise extends React.Component {
   render() {
-    return <div> Exercise</div>;
+    return (
+      <div>
+        <CreateProgramButton> Create a new program!</CreateProgramButton>
+      </div>
+    );
   }
 }
+
+const CreateProgramButton = styled.button`
+  height: 200px;
+  width: 200px;
+  border: 2px solid black;
+`;
 
 export default exercise;
